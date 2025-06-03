d0bdcb7ef736bbe4d5778a8613928b7c4f7cd948
Yelyzaveta Kriachko <lizakryachko@gmail.com>
2025-06-03 21:51:40 +0300

    Normal is finished

--- js-operators/assets/js/dist/main.dev.js
+++ js-operators/assets/js/dist/main.dev.js
@@ -23,3 +23,22 @@ function task3() {
 }
 
 ;
+
+function task4() {
+  var money = parseFloat(prompt('Введіть кількість грошей в вашому гаманці'));
+  var chocolatePrice = parseFloat(prompt('Введіть ціну однієї шоколадки'));
+  var qntChocolate = Math.trunc(money / chocolatePrice);
+  var change = money - qntChocolate * chocolatePrice;
+  alert("\u0412\u0438 \u0437\u043C\u043E\u0436\u0435\u0442\u0435 \u043A\u0443\u043F\u0438\u0442\u0438 ".concat(qntChocolate, " \u0448\u043E\u043A\u043E\u043B\u0430\u0434\u043E\u043A \u0456 \u0443 \u0432\u0430\u0441 \u0437\u0430\u043B\u0438\u0448\u0435\u0442\u044C\u0441\u044F ").concat(change, " \u0433\u0440\u043E\u0448\u0435\u0439"));
+}
+
+;
+
+function task5() {
+  var anyNum = parseInt(prompt('Введіть будь-яке тризначне число'));
+  var lustNum = (anyNum % 10).toFixed(0);
+  var midNum = Math.trunc(anyNum / 10) % 10;
+  var firstNum = Math.trunc(anyNum / 100);
+  var turnedNum = lustNum + midNum + firstNum;
+  alert("\u041F\u0435\u0440\u0435\u0432\u0435\u0440\u043D\u0443\u0442\u0435 \u0447\u0438\u0441\u043B\u043E \u0431\u0443\u0434\u0435 ".concat(turnedNum));
+}
--- js-operators/assets/js/main.js
+++ js-operators/assets/js/main.js
@@ -15,3 +15,20 @@ function task3 (){
     const pcsFiles = parseInt(flashCapacity * 1000 / 820);
     alert(`На вашу флешку вміститься ${pcsFiles} файлів об'ємом 820Мб`);
 };
+
+function task4 (){
+    const money = parseFloat(prompt('Введіть кількість грошей в вашому гаманці'));
+    const chocolatePrice = parseFloat(prompt('Введіть ціну однієї шоколадки'));
+    const qntChocolate = Math.trunc(money / chocolatePrice);
+    const change = money - qntChocolate * chocolatePrice;
+    alert(`Ви зможете купити ${qntChocolate} шоколадок і у вас залишеться ${change} грошей`);
+};
+
+function task5 (){
+    const anyNum = parseInt(prompt('Введіть будь-яке тризначне число'));
+    const lustNum = (anyNum % 10).toFixed(0);
+    const midNum = Math.trunc(anyNum / 10) % 10;
+    const firstNum = Math.trunc(anyNum / 100);
+    const turnedNum = lustNum + midNum + firstNum;
+    alert(`Перевернуте число буде ${turnedNum}`);
+}
