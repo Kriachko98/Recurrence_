61ddec516a67c445a9a6c1f706d616a1a59be89c
Yelyzaveta Kriachko <lizakryachko@gmail.com>
2026-04-21 18:36:57 +0300

    Map is finished

---
+++ selhono/assets/js/dist/map.dev.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function initMap(link) {
+  link.remove();
+  var map = L.map('map').setView([40.691576, -73.975210], 16);
+  L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>'
+  }).addTo(map);
+  var customMarker = L.icon({
+    iconUrl: 'assets/img/map-point.svg',
+    iconSize: [38, 56] // size of the icon
+
+  });
+  L.marker([40.691348345592836, -73.98052061744247], {
+    icon: customMarker
+  }).addTo(map);
+}
+
+;
